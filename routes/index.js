74e7f8600eac6c25a0779900099fc68cb9c21316
frasep <sebastien.poussart@sas.com>
2022-10-28 16:21:41 +0200

    Add new ddc

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,11 @@ router.get('/google_OrgChart.html', function(req, res, next) {
   res.render('google_OrgChart', { title: 'google_OrgChart ddc' });
 });
 
+router.get('/google_Calendar.html', function(req, res, next) {
+  res.render('google_Calendar', { title: 'google_Calendar ddc' });
+});
+
+
 router.get('/d3_radialStackedBar.html', function(req, res, next) {
   res.render('d3_radialStackedBar', { title: 'd3_radialStackedBar ddc' });
 });
@@ -26,5 +31,12 @@ router.get('/d3_FunnelChart.html', function(req, res, next) {
   res.render('d3_FunnelChart', { title: 'd3_FunnelChart ddc' });
 });
 
+router.get('/d3_circlePacking.html', function(req, res, next) {
+  res.render('d3_circlePacking', { title: 'd3_circlePacking ddc' });
+});
+
+router.get('/d3_spiralHeatmap.html', function(req, res, next) {
+  res.render('d3_spiralHeatmap', { title: 'd3_spiralHeatmap ddc' });
+});
 
 module.exports = router;
